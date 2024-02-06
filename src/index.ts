d061f826f55f13c223153d2bbb0deaa6f57d4240
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-06 22:09:53 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import * as actionsCore from '@actions/core';
-import { fetchManifestData } from './fetch_manifest_data.ts';
+import * as actionsArtifact from '@actions/artifact';
 
-const inputManifestURL = actionsCore.getInput('manifest-url');
+const artifact = new actionsArtifact.DefaultArtifactClient();
+
+// import { fetchManifestData } from './fetch_manifest_data.ts';
+
+// const inputManifestURL = actionsCore.getInput('manifest-url');
 
 (async () => {
-  console.log((await fetchManifestData(inputManifestURL)).latest);
+  // console.log((await fetchManifestData(inputManifestURL)).latest);
+  
+  const artifacts = await artifact.listArtifacts({latest: true})
+  artifacts.artifacts.forEach(({createdAt, id, name, size}) => {
+    console.log(createdAt, id, name, size);
+  })
 })()
 
