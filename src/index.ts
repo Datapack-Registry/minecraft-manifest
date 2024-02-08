1c6511a616e9b01286e5b4805784251ae1b90eee
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-08 23:10:19 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,13 @@ const githubToken = actionsCore.getInput('token');
 
 (async () => {
   console.log('Listing Artifacts...');
-  (await artifactClient.listArtifacts()).artifacts.forEach((artifact) => {
-    console.log('Artifact found:', artifact.createdAt, artifact.name, artifact.id, artifact.size);
+  (await actionsGithub.getOctokit(githubToken).rest.actions.listArtifactsForRepo({
+    owner: actionsGithub.context.repo.owner,
+    repo: actionsGithub.context.repo.repo
+  })).data.artifacts.forEach((artifact) => {
+    console.log('Workflow found:', artifact);
   })
   console.log('Listing Artifacts... Done!');
-  
-  console.log('Listing Workflows...');
-  (await actionsGithub.getOctokit(githubToken).rest.actions.listRepoWorkflows({owner: actionsGithub.context.repo.owner, repo: actionsGithub.context.repo.repo})).data.workflows.forEach((workflow) => {
-    console.log('Workflow found:', workflow);
-  })
-  console.log('Listing Workflows... Done!');
-  
-  
 
   const currentManifest = {
     latest: '',
