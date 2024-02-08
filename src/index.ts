8f13ec38b8cf6696658567c88730f109ef3e60ce
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-08 21:19:17 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,44 @@
 import * as actionsCore from '@actions/core';
 import * as cationsGithub from '@actions/github';
 import * as actionsArtifact from '@actions/artifact';
+import * as actionsToolCach from '@actions/tool-cache';
 import * as fs from 'fs/promises';
 
-const artifactClient = new actionsArtifact.DefaultArtifactClient();
-
 import { fetchManifestData } from './fetch_manifest_data.ts';
+
+
+const artifactClient = new actionsArtifact.DefaultArtifactClient();
 
 const inputManifestURL = actionsCore.getInput('manifest-url');
 
 (async () => {
+  const previousManifest = JSON.parse(await fs.readFile(actionsToolCach.find('latestManifest', '0.1.0'), {encoding: 'utf-8'}) || '{}');
+
+  console.log('previousManifest:', previousManifest);
+
   const latestManifest = (await fetchManifestData(inputManifestURL)).latest
-
-  console.log(latestManifest);
   
-
+  console.log('latestManifest:', latestManifest);
+  
+  
   fs.mkdir(`./data`, {recursive: true})
   await fs.writeFile(
-    `./data/latest_manifest.json`,
+    './data/latest_manifest.json',
     JSON.stringify(latestManifest)
   )
   
-
-  artifactClient.uploadArtifact(
-    'manifest',
-    [`./data/latest_manifest.json`],
-    `./data`
+  actionsToolCach.cacheFile(
+    './data/latest_manifest.json',
+    'latest_manifest.json',
+    'latestManifest',
+    '0.1.0'
   )
+
+  // artifactClient.uploadArtifact(
+  //   'manifest',
+  //   [`./data/latest_manifest.json`],
+  //   `./data`
+  // )
   
   // const artifacts = await artifactClient.listArtifacts({latest: true})
   // artifacts.artifacts.forEach(({createdAt, id, name, size}) => {
