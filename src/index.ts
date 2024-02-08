917b16bb968e8400f9c9252d8aff629d7fe24fcf
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-08 22:19:10 +0100

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as actionsCore from '@actions/core';
 import * as actionsGithub from '@actions/github';
 import * as actionsArtifact from '@actions/artifact';
-import * as actionsToolCach from '@actions/tool-cache';
 import * as fs from 'fs/promises';
 
 import { fetchManifestData } from './fetch_manifest_data.ts';
@@ -13,38 +12,23 @@ const inputManifestURL = actionsCore.getInput('manifest-url');
 const githubToken = actionsCore.getInput('token');
 
 (async () => {
-  // const previousManifest = JSON.parse(await fs.readFile(actionsToolCach.find('latestManifest', '0.1.0'), {encoding: 'utf-8'}) || '{}');
+  // download artifact from previous run and compare it to currentManifest data...
+  console.log('workflow:', actionsGithub.context.workflow);
 
-  // console.log('previousManifest:', previousManifest);
-  console.log('previousManifest:', actionsToolCach.find('latestManifest', '0.1.0'));
-
-  const latestManifest = (await fetchManifestData(inputManifestURL)).latest
+  const currentManifest = (await fetchManifestData(inputManifestURL)).latest
   
-  console.log('latestManifest:', latestManifest);
-  
+  console.log('latestManifest:', currentManifest);
   
   fs.mkdir(`./data`, {recursive: true})
   await fs.writeFile(
     './data/latest_manifest.json',
-    JSON.stringify(latestManifest)
-  )
-  
-  actionsToolCach.cacheFile(
-    './data/latest_manifest.json',
-    'latest_manifest.json',
-    'latestManifest',
-    '0.1.0'
+    JSON.stringify(currentManifest)
   )
 
-  // artifactClient.uploadArtifact(
-  //   'manifest',
-  //   [`./data/latest_manifest.json`],
-  //   `./data`
-  // )
-  
-  // const artifacts = await artifactClient.listArtifacts({latest: true})
-  // artifacts.artifacts.forEach(({createdAt, id, name, size}) => {
-  //   console.log(createdAt, id, name, size);
-  // })
+  artifactClient.uploadArtifact(
+    'manifest',
+    [`./data/latest_manifest.json`],
+    `./data`
+  )
 })()
 
