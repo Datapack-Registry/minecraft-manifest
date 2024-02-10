29d5216a4af6ba465dd6cdcb4153cd871c1ada0a
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-10 23:28:33 +0100

    Fix: Wrong Path

--- src/write_manifest_file.ts
+++ src/write_manifest_file.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs/promises';
 import { Latest } from './interface/latest.ts';
 
 export async function writeManifestFile(path : string, data : Latest) : Promise<void> {
-  await fs.mkdir(`./data`, {recursive: true}).catch((reason) => {
+  await fs.mkdir(path, {recursive: true}).catch((reason) => {
     console.log('Error while executing "mkdir":', reason);
   });
   await fs.writeFile(
