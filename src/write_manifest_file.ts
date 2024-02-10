9d850880a9e082ab9d50e5ee11e244f3c2ca3abc
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-10 23:51:27 +0100

    Fix: better error logging

--- src/write_manifest_file.ts
+++ src/write_manifest_file.ts
@@ -1,15 +1,18 @@
+import * as actionsCore from '@actions/core';
 import * as fs from 'fs/promises';
 import { dirname } from 'path';
 import { Latest } from './interface/latest.ts';
 
 export async function writeManifestFile(path : string, data : Latest) : Promise<void> {
   await fs.mkdir(dirname(path), {recursive: true}).catch((reason) => {
-    console.log('Error while executing "mkdir":', reason);
+    actionsCore.info('Error while executing "mkdir":');
+    actionsCore.error(reason);
   });
   await fs.writeFile(
     path,
     JSON.stringify(data)
   ).catch((reason) => {
-    console.log('Error while executing "writeFile":', reason);
+    actionsCore.info('Error while executing "writeFile":');
+    actionsCore.error(reason);
   });
 }
